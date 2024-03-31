3b4bcbcce84fbee2097f2826d21b731bf6f53cdf
xv82 <xvj8276@gmail.com>
2024-04-01 10:28:36 +1300

    Configured URL for recipe meal type template

--- djangoscenario/recipes/urls.py
+++ djangoscenario/recipes/urls.py
@@ -1,5 +1,6 @@
 from django.urls import path
 from . import views
+from .views import meal_display
 
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name='recipes-home'),
@@ -7,4 +8,5 @@ urlpatterns = [
     path('recipe/create', views.RecipeCreateView.as_view(), name="recipes-create"),
     path('recipe/<int:pk>/update', views.RecipeUpdateView.as_view(), name="recipes-update"),
     path('recipe/<int:pk>/delete', views.RecipeDeleteView.as_view(), name="recipes-delete"),
+    path('recipes/meals/<int:meal_id>/', meal_display, name="recipes-recipes"),
 ]
