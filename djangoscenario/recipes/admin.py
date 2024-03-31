64339482ff9c7fe90c2d99319e4194de9f3ff55e
xv82 <xvj8276@gmail.com>
2024-04-01 11:17:19 +1300

    Fixed the dropdown menu to display recipes based on button clicked

--- djangoscenario/recipes/admin.py
+++ djangoscenario/recipes/admin.py
@@ -3,3 +3,6 @@ from . import models
 # Register your models here.
 
 admin.site.register(models.Recipe)
+admin.site.register(models.MealDiet)
+admin.site.register(models.MealType)
+admin.site.register(models.HolidayMeal)
