a12951712ac2a81367fd8d25854cb3e9d485dc41
xv82 <xvj8276@gmail.com>
2024-04-01 11:04:33 +1300

    Fix for users uploading images via recipe creation in recipe form

--- djangoscenario/recipes/urls.py
+++ djangoscenario/recipes/urls.py
@@ -1,7 +1,8 @@
 from django.urls import path
 from . import views
 from .views import meal_display
-
+from django.conf.urls.static import static
+from django.conf import settings
 urlpatterns = [
     path('', views.RecipeListView.as_view(), name='recipes-home'),
     path('recipe/<int:pk>', views.RecipeDetailView.as_view(), name="recipes-detail"),
@@ -9,4 +10,4 @@ urlpatterns = [
     path('recipe/<int:pk>/update', views.RecipeUpdateView.as_view(), name="recipes-update"),
     path('recipe/<int:pk>/delete', views.RecipeDeleteView.as_view(), name="recipes-delete"),
     path('recipe/meals/<int:meal_id>/', meal_display, name="recipes-recipes"),
-]
+] + static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
