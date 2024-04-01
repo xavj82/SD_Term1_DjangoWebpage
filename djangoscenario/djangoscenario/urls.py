ada9371fe6d052a6ec459e7cf4e162783422447c
xv82 <xvj8276@gmail.com>
2024-04-01 16:13:04 +1300

    Fixed typo in urls.py

--- djangoscenario/djangoscenario/urls.py
+++ djangoscenario/djangoscenario/urls.py
@@ -19,11 +19,12 @@ from django.urls import path, include
 from users import views as user_views
 from django.contrib.auth import views as auth_views
 
+
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', include('recipes.urls')),
     path('register/', user_views.register, name="user-register"),
-    path('login/', auth_views.LoginView.as_view(template_name='user-login.html'), name="user-login"),
+    path('login/', auth_views.LoginView.as_view(template_name='users/login.html'), name="user-login"),
     path('logout/', auth_views.LogoutView.as_view(template_name='users/logout.html'), name='user-logout'),
     path('profile/', user_views.profile, name="user-profile"),
 ]
